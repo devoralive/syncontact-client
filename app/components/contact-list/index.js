c9546a427811fe5d8a237ae6b8aaecd509fed1fc
Jeremy Berthelier <jeremyberthelier@hotmail.fr>
2018-04-28 23:13:08 +0200

    Small update to contact liste with native base

--- app/components/contact-list/index.js
+++ app/components/contact-list/index.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import { StackNavigator } from 'react-navigation';
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
+import { Container, Header, Content, List, ListItem, Text } from 'native-base';
 
 class ContactList extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Page d'accueil : Liste des contacts</Text>
-        <Button
-          title="Jérémy Berthelier"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
+      <Container>
+        <Content>
+          <List>
+            <ListItem itemDivider>
+              <Text>A</Text>
+            </ListItem>                    
+            <ListItem >
+              <Text>Aaron Bennet</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Ali Connors</Text>
+            </ListItem>
+            <ListItem itemDivider>
+              <Text>B</Text>
+            </ListItem>  
+            <ListItem>
+              <Text 
+                onPress={() => this.props.navigation.navigate('Details')}
+              >
+                Jérémy Berthelier
+              </Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
