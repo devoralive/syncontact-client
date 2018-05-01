e3fed60a28e2fb56205cda87952fc10d66d6c639
Jeremy Berthelier <jeremyberthelier@hotmail.fr>
2018-05-01 13:04:10 +0200

    Add fake users and print them in details contact

--- app/components/contact-card/index.js
+++ app/components/contact-card/index.js
@@ -5,12 +5,35 @@ import FlexImage from 'react-native-flex-image';
 
 const { height, width} = Dimensions.get("window")
 
+const users = [
+  { "id": 1, "first_name": "Marielle", "last_name": "Allpower", "job_title": "Ebeniste", "job_compagny": "Ateliers des 4 mains"},
+  { "id": 2, "first_name": "Welch", "last_name": "Alcapone", "job_title": "Dealer d'herbe à chat", "job_compagny": "La rue"},
+  { "id": 3, "first_name": "Kay", "last_name": "Altère", "job_title": "Champion Olympique de ronflette", "job_compagny": "Association des 42"},
+  { "id": 4, "first_name": "Jérémy", "last_name": "Berthelier", "job_title": "Développeur web", "job_compagny": "Assurinco"},
+  { "id": 5, "first_name": "Darrelle", "last_name": "Blondin", "job_title": "Professeur en mathématique", "job_compagny": "Collège des petits nains"},
+  { "id": 6, "first_name": "Pavla", "last_name": "Bernard", "job_title": "Philosophe des temps anciens", "job_compagny": "Sous l'océan"}
+]
+
 class ContactCard extends React.Component {
   static navigationOptions = {
     title: 'Jérémy Berthelier',
   };
 
   render() {
+    /* Read the params from the navigation state */
+    const { params } = this.props.navigation.state;
+    const id = params ? params.id : null;
+    const contact = users.find((user) => user.id === id)
+
+    const imagesProfil = [
+      require(`../../assets/image/profil-1.jpg`),
+      require(`../../assets/image/profil-2.jpg`),
+      require(`../../assets/image/profil-3.jpg`),
+      require(`../../assets/image/profil-4.jpg`),
+      require(`../../assets/image/profil-5.jpg`),
+      require(`../../assets/image/profil-6.jpg`)
+    ]
+
     return (
       <View 
         style={{
@@ -24,7 +47,7 @@ class ContactCard extends React.Component {
           }}
         >
           <FlexImage
-            source={require("../../assets/image/profil-img.jpg")}
+            source={imagesProfil[contact.id - 1]}            
           />
         </View>
 
@@ -35,9 +58,9 @@ class ContactCard extends React.Component {
             alignItems: "center"
           }}
         >
-          <H1>Jérémy Berthelier</H1>
-          <Text>Travaille à : Assurinco</Text>
-          <Text>Dévellopeur web</Text>
+          <H1>{contact.first_name} {contact.last_name}</H1>
+          <Text>Travaille à : {contact.job_compagny}</Text>
+          <Text>{contact.job_title}</Text>
           <Button 
             rounded
             success
--- app/components/contact-list/index.js
+++ app/components/contact-list/index.js
@@ -9,29 +9,47 @@ class ContactList extends React.Component {
   };
 
   render() {
+    const filterBeginWith = (letter, element) => {
+      if (element.last_name.toLowerCase().charAt(0) === letter) {
+        return element
+      }
+    }
+
+    const users = [
+      { "id": 1, "first_name": "Marielle", "last_name": "Allpower", "job_title": "Ebeniste", "job_compagny": "Ateliers des 4 mains"},
+      { "id": 2, "first_name": "Welch", "last_name": "Alcapone", "job_title": "Dealer d'herbe à chat", "job_compagny": "La rue"},
+      { "id": 3, "first_name": "Kay", "last_name": "Altère", "job_title": "Champion Olympique de ronflette", "job_compagny": "Association des 42"},
+      { "id": 4, "first_name": "Jérémy", "last_name": "Berthelier", "job_title": "Développeur web", "job_compagny": "Assurinco"},
+      { "id": 5, "first_name": "Darrelle", "last_name": "Blondin", "job_title": "Professeur en mathématique", "job_compagny": "Collège des petits nains"},
+      { "id": 6, "first_name": "Pavla", "last_name": "Bernard", "job_title": "Philosophe des temps anciens", "job_compagny": "Sous l'océan"}
+    ]
+    
+    const alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    alpha.split("").map( (letter) => {
+      let group = users.filter(filterBeginWith.bind(this, letter))
+    })
+
+
     return (
       <Container>
         <Content>
           <List>
-            <ListItem itemDivider>
-              <Text>A</Text>
-            </ListItem>                    
-            <ListItem >
-              <Text>Aaron Bennet</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Ali Connors</Text>
-            </ListItem>
-            <ListItem itemDivider>
-              <Text>B</Text>
-            </ListItem>  
-            <ListItem>
-              <Text 
-                onPress={() => this.props.navigation.navigate('Details')}
-              >
-                Jérémy Berthelier
-              </Text>
-            </ListItem>
+            {alpha.split("").map( (letter) => {
+              let group = users.filter(filterBeginWith.bind(this, letter))
+              return (
+                <View>
+                  <ListItem itemDivider key={letter}><Text>{letter.toUpperCase()}</Text></ListItem>
+                  {group.map( (contact) => 
+                    <ListItem key={contact.id}>
+                      <Text
+                        onPress={() => 
+                          this.props.navigation.navigate('Details', {id: contact.id})
+                        }>{contact.last_name} {contact.first_name}</Text>
+                    </ListItem>)}
+                </View>
+              )
+            })}
           </List>
         </Content>
       </Container>
